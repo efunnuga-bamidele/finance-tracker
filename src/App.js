c53f2ca85d4256401921793186d02be32e036876
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-10-10 22:24:18 +0100

    🚀 create app structure, pages and form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import './App.css';
+
+//pages & components
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/signup' element={<Signup />}></Route>
+        </Routes>
+      </BrowserRouter>
+     
     </div>
   );
 }
---
+++ src/pages/signup/Signup.js
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+//Style
+import styles from './Signup.module.css'
+
+export default function Signup() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [displayName, setDisplayName] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(email, password, displayName)
+
+    }
+    return(
+        <form className={styles['signup-form']} onSubmit={handleSubmit}>
+        <h2>Signup</h2>
+            <label>
+                <span>Email:</span>
+                <input 
+                    type='email'
+                    placeholder='Enter your email address'
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+            </label>
+            <label>
+                <span>Password:</span>
+                <input
+                    type='password'
+                    placeholder='Enter password'
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
+            </label>
+            <label>
+                <span>Display Name:</span>
+                <input
+                    type='text'
+                    placeholder='Enter display name'
+                    onChange={(e) => setDisplayName(e.target.value)}
+                    value={displayName}
+                />
+            </label>
+            <button className='btn'>Sign up</button>
+        </form>
+    )
+}
